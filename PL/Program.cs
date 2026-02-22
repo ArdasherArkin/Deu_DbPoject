40c6bf182fb7fc7e49566d24241274ab438567b4
arkinstone <ardasher.bobokhonov@gmail.com>
2026-02-22 17:18:34 +0300

    seccond with addings

--- PL/Program.cs
+++ PL/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int how = 5;
+for(int i = 0; i < 4; i++)
+{
+    Console.Write("{1} \t", how);
+}
